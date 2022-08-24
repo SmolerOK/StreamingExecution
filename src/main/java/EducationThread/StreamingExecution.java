b23214ad5cbb857d20505d2902741eab6af5f898
Sergey <serj.rexif@gmail.com>
2022-08-24 14:11:59 +0300

    Converted 4 streams into 2 methods

--- src/main/java/EducationThread/StreamingExecution.java
+++ src/main/java/EducationThread/StreamingExecution.java
@@ -5,30 +5,34 @@ import java.io.IOException;
 
 public class StreamingExecution {
     public static void main(String[] args) {
-        int[] arrayFirst = new int[250_000];
-        int[] arrayTwo = new int[250_000];
-        int[] arrayThree = new int[250_000];
-        int[] arrayFour = new int[250_000];
+        double[] arrayFirst = new double[250_000];
+        double[] arrayTwo = new double[250_000];
+        double[] arrayThree = new double[250_000];
+        double[] arrayFour = new double[250_000];
 
-        new Thread(() -> {
+        CalculatingRoot(arrayFirst, arrayTwo);
 
-        }).start();
-
-        new Thread(() -> {
-
-        }).start();
-
-        new Thread(() -> {
-
-        }).start();
-
-        new Thread(() -> {
-
-        }).start();
+        CalculatingRoot(arrayThree, arrayFour);
 
         try (FileWriter fileWriter = new FileWriter("C:\\Users\\Sergey\\IdeaProjects\\EducationThread\\WriteText.txt", true)) {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static void CalculatingRoot(double[] arrayListA, double[] arrayListB) {
+        new Thread(() -> {
+            for (int i = 0; i < arrayListA.length; i++) {
+                double result = Math.sqrt(arrayListA[i]);
+                arrayListA[i] = result;
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < arrayListB.length; i++) {
+                double result = Math.sqrt(arrayListB[i]);
+                arrayListB[i] = result;
+            }
+        }).start();
     }
 }
