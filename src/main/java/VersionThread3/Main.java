b0bd8986966f8a1157bfc4b94710701c5e2edf01
Sergei <sergei.smoler@mail.ru>
2022-09-01 01:11:16 +0300

    Created VersionThread3

--- src/main/java/VersionThread3/Main.java
+++ src/main/java/VersionThread3/Main.java
@@ -1,7 +1,34 @@
 package VersionThread3;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Main {
+    private final static double[] mainArr = new double[1_000_000];
+
+    public static void main(String[] args) throws InterruptedException {
+        createThread(1);
+        createThread(2);
+        createThread(3);
+
+        Thread.sleep(1000);
+
+        try (FileWriter writer = new FileWriter("C:\\Users\\Sergei\\IdeaProjects\\StreamingExecution\\src\\main\\java\\VersionThread3\\Text.txt", false)) {
+            for (int i = 0; i < mainArr.length; i++) {
+                writer.write(i + " = " + mainArr[i] + "\n");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void createThread(int threadNum) {
+        new Thread(() -> {
+            int count = mainArr.length / 3;
+            for (int i = 0; i < count; i++) {
+                int arrIndex = mainArr.length - count * threadNum + i;
+                mainArr[arrIndex] = Math.sqrt(arrIndex);
+            }
+        }).start();
     }
 }
